619dc61c55af2b870707c43b6571d7b5ae79e4e8
changming.jiang <394358458@qq.com>
2018-11-07 14:58:20 +0800

    升级各软件版本

--- buildRedisOrSentinel.sh
+++ buildRedisOrSentinel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker build redis/ -t changmingjiang/redis:latest
+docker build redis/ --build-arg "HTTPS_PROXY=http://10.2.236.176:1087" -t changmingjiang/redis:latest
 docker build redis-sentinel/ -t changmingjiang/redis-sentinel:latest
 
 docker tag changmingjiang/redis:latest changmingjiang/redis:5.0
