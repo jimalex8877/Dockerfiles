18a7942d06062670b98b42d05dd016557439bcab
changming.jiang <394358458@qq.com>
2018-11-16 14:37:34 +0800

    升级rabbitmq版本

--- buildRabbitMQ.sh
+++ buildRabbitMQ.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker build rabbitMQ/ --build-arg "HTTPS_PROXY=http://10.2.236.176:1087" -t changmingjiang/rabbitmq:latest
 
-docker tag changmingjiang/rabbitmq:latest changmingjiang/rabbitmq:3.7.8
+docker tag changmingjiang/rabbitmq:latest changmingjiang/rabbitmq:3.7.9
