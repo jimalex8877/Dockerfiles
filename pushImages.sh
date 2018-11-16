744570c4c18e8da77a002388a55312ccfbacfdaf
changming.jiang <394358458@qq.com>
2018-11-16 18:07:21 +0800

    push

--- pushImages.sh
+++ pushImages.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 docker push changmingjiang/debian:latest
+
 docker push changmingjiang/redis:latest
+docker push changmingjiang/redis:5.0.1
+
 docker push changmingjiang/redis-sentinel:latest
-docker push changmingjiang/idea-license-server:latest
+docker push changmingjiang/redis-sentinel:5.0.1
+
+#docker push changmingjiang/idea-license-server:latest
+
 docker push changmingjiang/openjdk8-jre:latest
+
 docker push changmingjiang/mongodb:latest
+docker push changmingjiang/mongodb:4.0.4
+
 docker push changmingjiang/postgres:latest
+docker push changmingjiang/postgres:11
+
 docker push changmingjiang/rabbitmq:latest
+docker push changmingjiang/rabbitmq:3.7.9
+
 docker push changmingjiang/nginx:latest
-docker push changmingjiang/zookeeper:latest
-docker push changmingjiang/kafka:latest
+docker push changmingjiang/nginx:1.14.1
+
+#docker push changmingjiang/zookeeper:latest
+#docker push changmingjiang/kafka:latest
