4c1802335991448b5d7050b17d88078802d79876
changming.jiang <394358458@qq.com>
2018-11-26 15:01:31 +0800

    升级consul，集群搭建未完待续

--- buildConsul.sh
+++ buildConsul.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build consul/ --build-arg "HTTPS_PROXY=http://10.2.236.176:1087" -t changmingjiang/consul:latest
+docker build consul/ -t changmingjiang/consul:latest
 
-docker tag changmingjiang/consul:latest changmingjiang/consul:1.3.0
+docker tag changmingjiang/consul:latest changmingjiang/consul:1.4.0
