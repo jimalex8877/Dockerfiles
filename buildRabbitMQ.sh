72f07a9d383e7a8bacd4197fd2883ed0766f279d
changming.jiang <changming.jiang@qq.com>
2019-07-23 13:50:36 +0800

    update

--- buildRabbitMQ.sh
+++ buildRabbitMQ.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker build rabbitMQ/ -t changmingjiang/rabbitmq:latest
 
-docker tag changmingjiang/rabbitmq:latest changmingjiang/rabbitmq:3.7.15
+docker tag changmingjiang/rabbitmq:latest changmingjiang/rabbitmq:3.7.16
