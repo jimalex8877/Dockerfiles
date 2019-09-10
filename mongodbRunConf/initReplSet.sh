400c19ecfe5474999dc164419b2a565114ffd530
changming.jiang <changming.jiang@qq.com>
2019-09-10 16:08:58 +0800

    更新mongo运行示例配置

--- mongodbRunConf/initReplSet.sh
+++ mongodbRunConf/initReplSet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker exec -it mongodb-27017 mongo --port 27017
-#rs.initiate({_id:"changming", members:[{_id:0,host:"10.2.236.140:27017"},{_id:1,host:"10.2.236.140:37017"},{_id:2,host:"10.2.236.140:47017",arbiterOnly:true}]})
-#rs.reconfig({_id:"changming", members:[{_id:0,host:"10.2.236.140:27017"},{_id:1,host:"10.2.236.140:37017"},{_id:2,host:"10.2.236.140:47017",arbiterOnly:true}]},{force:true})
+#rs.initiate({_id:"changming", members:[{_id:0,host:"10.2.239.216:27017"},{_id:1,host:"10.2.239.216:37017"},{_id:2,host:"10.2.239.216:47017",arbiterOnly:true}]})
+#rs.reconfig({_id:"changming", members:[{_id:0,host:"10.2.239.216:27017"},{_id:1,host:"10.2.239.216:37017"},{_id:2,host:"10.2.239.216:47017",arbiterOnly:true}]},{force:true})
 
 #rs.initiate({_id:"changming", members:[{_id:0,host:"192.168.1.103:27017"},{_id:1,host:"192.168.1.103:37017"},{_id:2,host:"192.168.1.103:47017",arbiterOnly:true}]})
 #rs.reconfig({_id:"changming", members:[{_id:0,host:"192.168.1.103:27017"},{_id:1,host:"192.168.1.103:37017"},{_id:2,host:"192.168.1.103:47017",arbiterOnly:true}]},{force:true})
