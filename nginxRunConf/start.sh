ed2a9bd74ec2a0ed47640a27bfee400b4939ae1a
changming.jiang <394358458@qq.com>
2020-04-14 17:56:37 +0800

    还原

--- nginxRunConf/start.sh
+++ nginxRunConf/start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p `pwd`/conf
-mkdir -p `pwd`/logs
 docker run --restart=always -d --name nginx \
 	--expose 80 -p 80:80 \
 	--expose 443 -p 443:443 \
-	-v `pwd`/conf/:/opt/nginx/conf.d/ \
-	-v `pwd`/logs/:/opt/nginx/logs/ \
+	-v `pwd`/conf/:/etc/nginx/conf.d/ \
 	changmingjiang/nginx:latest
